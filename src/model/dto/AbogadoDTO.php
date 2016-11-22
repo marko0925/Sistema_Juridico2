11446e0e2cff8fd3cff2adf84b7b2bd35cc747b0
k1ngd00m <miguelangelprolive@gmail.com>
2016-11-21 20:51:08 -0500

    Agregando funcionalidad AbogadoDTO

--- src/model/dto/AbogadoDTO.php
+++ src/model/dto/AbogadoDTO.php
@@ -1,44 +1,31 @@
 <?php
-include_once __DIR__.'/PersonaDTO.php';
+require_once __DIR__.'/../dto/EspecialidadDTO.php';
+require_once __DIR__.'/../dto/PersonaDTO.php';
 /**
  * Created by PhpStorm.
  * User: McBro
  * Date: 16/11/2016
  * Time: 4:40 PM
  */
-class AbogadoDTO extends PersonaDTO
+class AbogadoDTO extends PersonaDTO implements JsonSerializable
 {
     private $especialidad;
     private $almamater;
 
     /**
      * AbogadoDTO constructor.
+     * @param $especialidad
+     * @param $almamater
      */
-    public function __construct($dni, $nombre, $apellido, $correo, $fecha_nac, $telefono, $especialidad, $almamater)
+    public function __construct($especialidad=null, $almamater=null)
     {
-        parent::__construct($dni,$nombre,$apellido,$correo,$fecha_nac,$telefono);
+        parent::__construct();
         $this->especialidad = $especialidad;
         $this->almamater = $almamater;
     }
 
     /**
-     * @return mixed
-     */
-    public function getAlmamater()
-    {
-        return $this->almamater;
-    }
-
-    /**
-     * @param mixed $almamater
-     */
-    public function setAlmamater($almamater)
-    {
-        $this->almamater = $almamater;
-    }
-
-    /**
-     * @return mixed
+     * @return array
      */
     public function getEspecialidad()
     {
@@ -46,11 +33,44 @@ class AbogadoDTO extends PersonaDTO
     }
 
     /**
-     * @param mixed $especialidad
+     * @param array $especialidad
      */
     public function setEspecialidad($especialidad)
     {
         $this->especialidad = $especialidad;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlmamater()
+    {
+        return $this->almamater;
+    }
+
+    /**
+     * @param string $almamater
+     */
+    public function setAlmamater($almamater)
+    {
+        $this->almamater = $almamater;
+    }
+
+    /**
+     * Metodo- sirve para convetir un objeto con atributos privados en una cadena con formato json usando el metodo json_encode
+     * @return array
+     */
+    public function jsonSerialize() {
+        return [
+            'dni' => $this->getDni(),
+            'apellido' => $this->getApellido(),
+            'nombre' => $this->getNombre(),
+            'fechaNacimiento' => $this->getFecha_nac(),
+            'telefono' => $this->getTelefono(),
+            'email' => $this->getCorreo(),
+            'almamater'=>$this->getAlmamater(),
+            'especialidad'=>$this->getEspecialidad()
+        ];
     }
 
 }
