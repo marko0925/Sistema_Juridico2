fdcc42a280a50ee2607207f14fe2a579849ee2ad
k1ngd00m <miguelangelprolive@gmail.com>
2016-11-21 20:57:45 -0500

    Actualizando AbogadoService

--- src/model/services/AbogadoService.php
+++ src/model/services/AbogadoService.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/../factory/TransactionManager.php';
-require_once __DIR__ . '/../dao/AbogadoDAO.php';
-
+require_once __DIR__.'/../dao/AbogadoDAO.php';
 /**
  * Created by PhpStorm.
  * User: McBro
@@ -10,36 +9,100 @@ require_once __DIR__ . '/../dao/AbogadoDAO.php';
  */
 class AbogadoService
 {
-
+    /**
+     * @param AbogadoDTO $dto
+     */
     public function registrar($dto)
     {
         try {
-            $transaccion = new TransactionManager(true);
-            $transaccion->beginTransaction();
-
-            $abogadoDAO = $transaccion->getDAO("AbogadoDAO");
-            $abogadoDAO->registrar();
+            $manager = new TransactionManager(true);
+            //registro abogado
+            $abogadoDAO = $manager->getDAO("AbogadoDAO");
+            $abogadoDAO->registrar($dto);
+            //registro especialidad abogado
+            $especialidadDAO =$manager->getDAO('EspecialidadDAO');
+            $especialidadDAO->registrarListadoEspecialidad($dto->getDni(),$dto->getEspecialidad());
+            $manager->flush();
         } catch (Exception $e) {
-            if (isset($transaccion)) {
-                $transaccion->rollback();
+
+            echo $e;
+            if (isset($manager)) {
+                $manager->rollback();
+            }
+        }
+        finally{
+
+            if (isset($manager)) {
+                $manager->close();
             }
         }
     }
 
-    public function listarAbogados()
-    {
+    /**
+     * Metodo- elimina una de las especialidades de una abogado
+     * @param  AbogadoDTO $dto
+     */
+    public function actualizar($dto){
         try {
-            return [ "abogados"=>["dni"=>"88554434","nombre" => "Javier", "apellido" => "Rodriguez", "correo" => "javier@gmail.com", "fecha_nac" => "15/08/91", "telefono" => 5762777,
-                "especialidad" => "Casos Familiares", "almamater" => "UFPS"],
-                ["dni"=>"09423423","nombre" => "Carlos", "apellido" => "Fernandez", "correo" => "carlos@gmail.com", "fecha_nac" => "12/02/76", "telefono" => 5872133,
-                "especialidad" => "Casos Militares", "almamater" => "UDES"]];
-            // $transaccion = new TransactionManager(true);
-            // $transaccion->beginTransaction();
+            $manager = new TransactionManager(true);
+            //actualiza abogado
+            $abogadoDAO = $manager->getDAO("AbogadoDAO");
+            $abogadoDAO->actualizar($dto);
+            //actualiza especialidades abogado
+            $especialidadDAO=$manager->getDAO('EspecialidadDAO');
+            $especialidadDAO->eliminarEspecialidad($dto->getDni());
+            if($dto->getEspecialidad()!==null){
+                $especialidadDAO->registrarListadoEspecialidad($dto->getDni(),$dto->getEspecialidad());
+            }
 
-            //$abogadoDAO = $transaccion->getDAO("AbogadoDAO");
-            //$abogadoDAO->listar();
+            $manager->flush();
         } catch (Exception $e) {
 
+            echo $e;
+            if (isset($manager)) {
+                $manager->rollback();
+            }
         }
+        finally{
+
+            if (isset($manager)) {
+                $manager->close();
+            }
+        }
+
+    }
+
+    public function listado(){
+      $listado=null;
+      try {
+        $manager=new TransactionManager(true);
+        $dao=$manager->getDAO('AbogadoDAO');
+        $listado=$dao->listar();
+      } catch (Exception $e) {
+        echo $e;
+      }
+      finally{
+        if(isset($manager)){
+          $manager->close();
+        }
+      }
+      return $listado;
+
+    }
+    public function listarEspecializaciones($nit){
+      $listado=null;
+      try {
+        $manager=new TransactionManager(true);
+        $dao=$manager->getDAO('EspecialidadDAO');
+        $listado=$dao->findBy($nit);
+      } catch (Exception $e) {
+        echo $e;
+      }
+      finally{
+        if(isset($manager)){
+          $manager->close();
+        }
+      }
+      return $listado;
     }
 }
