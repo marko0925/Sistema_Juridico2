27132e2fa6ddeda10199d3ae25b7a8ec5c66dc55
k1ngd00m <miguelangelprolive@gmail.com>
2016-11-29 11:31:29 -0500

    cambios

--- src/model/dao/ExpedienteDAO.php
+++ src/model/dao/ExpedienteDAO.php
@@ -21,14 +21,34 @@ class ExpedienteDAO
     {
         $this->con=$con;
     }
+
+    /**
+     * @param ExpedienteDTO $dto
+     * @return mixed
+     */
     public function registrar($dto){
-        $sql="INSERT INTO _observacion(_id_observacion, _nota, _fecha, _nombre, _abogado_caso, _id_caso)VALUES (nextval('_observacion__id_observacion_seq'),?,?,?,?,?)";
-        $dtoAbogadoCaso=$dto->getAbogadoCaso();
-        $parametros=array($dto->getNota(),$dto->getFecha(),$dto->getNombre(),$dtoAbogadoCaso->getAbogado()->getDni(),$dtoAbogadoCaso->getProceso()->getIdCaso());
+        $sql="INSERT INTO _expediente(_id_expediente, _fecha, _url, _descripcion, _tipo) VALUES (nextvalue('_expediente__id_expediente_seq'), ?, ?, ?, ?)";
+        $parametros=array($dto->getFecha(),$dto->getUrl(),$dto->getDescripcion(),$dto->getTipo());
         if($this->con->save($sql,$parametros)){
-            $dto->setIdObservacion($this->con->lastInsertId('_observacion__id_observacion_seq'));
+            $dto->setIdExpediente($this->con->lastInsertId('_expediente__id_expediente_seq'));
         }
         return $dto;
+    }
+    public function listar($idProceso){
+        $sql='SELECT _id_expediente, _fecha, _url, _descripcion, _tipo FROM _expediente WHERE _proceso=?';
+        $parametros=array($idProceso);
+        $resultSet=$this->con->findBy($sql,$parametros);
+        $listado=  array();
+        foreach ($resultSet as $row){
+            $dto= new ExpedienteDTO();
+            $dto->setIdExpediente($row['_id_expediente']);
+            $dto->setFecha($row['_fecha']);
+            $dto->setUrl($row['_url']);
+            $dto->setDescripcion($row['_descripcion']);
+            $dto->setTipo($row['_tipo']);
+            $listado[]=$dto;
+        }
+        return $listado;
     }
 
 
