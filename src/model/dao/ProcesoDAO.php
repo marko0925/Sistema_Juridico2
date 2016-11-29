9a2e665979f2258a42a90d670aaead9aa0ae13b6
k1ngd00m <miguelangelprolive@gmail.com>
2016-11-29 11:24:32 -0500

    cambios

--- src/model/dao/ProcesoDAO.php
+++ src/model/dao/ProcesoDAO.php
@@ -23,8 +23,8 @@ class ProcesoDAO
      * @return ProcesoDTO
      */
     public function registrar($dto){
-        $sql="INSERT INTO _caso(_id_caso,_tipo,_fecha_ini,_fecha_fin,_nombre_juez,_dni)values(nextval('_caso__id_caso_seq'),?,?,?,?,?)";
-        $parametros=array($dto->getTipoCaso(),$dto->getFechaInicio(),$dto->getFechaFin(),$dto->getJuez(),$dto->getCliente()->getDni());
+        $sql="INSERT INTO _caso(_id_caso, _tipo, _fecha_ini, _fecha_fin, _nombre_juez, _dni, _descripcion, _radicado, _estado) VALUES (nextval('_caso__id_caso_seq'), ?, ?, ?, ?, ?, ?, ?, ?);";
+        $parametros=array($dto->getTipoCaso(),$dto->getFechaInicio(),$dto->getFechaFin(),$dto->getJuez(),$dto->getCliente()->getDni(),$dto->getDescripcion(),$dto->getRadicado(),$dto->isEstado());
         if($this->con->save($sql,$parametros)){
             $dto->setIdCaso($this->con->lastInsertId('_caso__id_caso_seq'));
         }
@@ -36,19 +36,47 @@ class ProcesoDAO
      * @return null|ProcesoDTO
      */
     public function buscar($idProceso){
-        $sql='SELECT * FROM _caso WHERE _id_caso= ?';
+        $sql='SELECT _id_caso, _tipo, _fecha_ini, _fecha_fin, _nombre_juez, _dni, _descripcion, _radicado, _estado FROM _caso WHERE _id_caso= ?';
         $parametros =array($idProceso);
         $result=$this->con->findByOne($sql,$parametros);
         $dto=null;
         if($result!==null){
-            $dto= new ProcesoDTO();
-            $dto->setIdCaso($result['_id_caso']);
-            $dto->setTipoCaso($result['_tipo']);
-            $dto->setFechaInicio($result['_fecha_ini']);
-            $dto->setFechaFin($result['_fecha_fin']);
-            $dto->setJuez($result['_nombre_juez']);
+
+            $dto= $this->getProcesoDTO($result['_id_caso'],$result['_tipo'],$result['_fecha_ini'],$result['_fecha_fin'],$result['_nombre_juez'],$result['_descripcion'],$result['_radicado'],$result['_estado']);
+
         }
         return $dto;
+    }
+
+    /**
+     *
+     */
+    public function listar(){
+        $sql='SELECT _id_caso, _tipo, _fecha_ini, _fecha_fin, _nombre_juez, _dni, _descripcion, _radicado, _estado FROM _caso';
+        $result=$this->con->findAll($sql);
+        $listado=[];
+        if($result!=null){
+            //print_r($result);
+            foreach ($result as $row){
+               $listado[]= $this->getProcesoDTO($row['_id_caso'],$row['_tipo'],$row['_fecha_ini'],$row['_fecha_fin'],$row['_nombre_juez'],$row['_descripcion'],$row['_radicado'],$row['_estado']);
+            }
+        }
+        return $listado;
+
+    }
+
+    private function getProcesoDTO($idCaso,$tipo,$fechaIni,$fechaFin,$juez,$descripcion,$radicado,$estado){
+        $dto=new ProcesoDTO();
+        $dto->setIdCaso($idCaso);
+        $dto->setTipoCaso($tipo);
+        $dto->setFechaInicio($fechaIni);
+        $dto->setFechaFin($fechaFin);
+        $dto->setJuez($juez);
+        $dto->setDescripcion($descripcion);
+        $dto->setRadicado($radicado);
+        $dto->setEstado($estado);
+        return $dto;
+
     }
 
 }
