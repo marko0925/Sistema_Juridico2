a138175f9871d7c8439c5fcc5bfa0fe585f0c2db
marko0925 <marlonyessid0925@gmail.com>
2016-11-29 19:37:41 -0500

    listado procesos actualizado

--- src/views/proceso/listadoProcesos.php
+++ src/views/proceso/listadoProcesos.php
@@ -1,72 +1,62 @@
-<div class="box box-primary">
-    <div class="box-header with-border">
-        <h3 class="box-title">Recently Added Products</h3>
-
-        <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-        </div>
+<div class="box">
+    <div class="box-header">
+        <h3 class="box-title">Abogados</h3>
     </div>
     <!-- /.box-header -->
     <div class="box-body">
-        <ul class="products-list product-list-in-box">
-            <li class="item">
-                <div class="product-img">
-                    <img src="dist/img/default-50x50.gif" alt="Product Image">
-                </div>
-                <div class="product-info">
-                    <a href="javascript:void(0)" class="product-title">Samsung TV
-                        <span class="label label-warning pull-right">$1800</span></a>
-                    <span class="product-description">
-                          Samsung 32" 1080p 60Hz LED Smart HDTV.
-                        </span>
-                </div>
-            </li>
-            <!-- /.item -->
-            <li class="item">
-                <div class="product-img">
-                    <img src="dist/img/default-50x50.gif" alt="Product Image">
-                </div>
-                <div class="product-info">
-                    <a href="javascript:void(0)" class="product-title">Bicycle
-                        <span class="label label-info pull-right">$700</span></a>
-                    <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
-                        </span>
-                </div>
-            </li>
-            <!-- /.item -->
-            <li class="item">
-                <div class="product-img">
-                    <img src="dist/img/default-50x50.gif" alt="Product Image">
-                </div>
-                <div class="product-info">
-                    <a href="javascript:void(0)" class="product-title">Xbox One <span class="label label-danger pull-right">$350</span></a>
-                    <span class="product-description">
-                          Xbox One Console Bundle with Halo Master Chief Collection.
-                        </span>
-                </div>
-            </li>
-            <!-- /.item -->
-            <li class="item">
-                <div class="product-img">
-                    <img src="dist/img/default-50x50.gif" alt="Product Image">
-                </div>
-                <div class="product-info">
-                    <a href="javascript:void(0)" class="product-title">PlayStation 4
-                        <span class="label label-success pull-right">$399</span></a>
-                    <span class="product-description">
-                          PlayStation 4 500GB Console (PS4)
-                        </span>
-                </div>
-            </li>
-            <!-- /.item -->
-        </ul>
+        <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+
+
+            <div class="col-sm-12">
+
+                <table id="lista-abogados" class="table table-bordered table-striped dataTable" role="grid"
+                       aria-describedby="example1_info">
+                    <thead>
+                    <tr role="row">
+                        <th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                            aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending"
+                            style="width: 177px;">Radicado
+                        </th>
+                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                            aria-label="Browser: activate to sort column ascending" style="width: 224px;">Cliente
+                        </th>
+                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                            aria-label="Platform(s): activate to sort column ascending" style="width: 205px;">
+                            Descripción
+                        </th>
+                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                            aria-label="Engine version: activate to sort column ascending" style="width: 152px;">
+                            Estado
+                        </th>
+                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                            aria-label="CSS grade: activate to sort column ascending" style="width: 110px;">Acciones
+                        </th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
     <!-- /.box-body -->
-    <div class="box-footer text-center">
-        <a href="javascript:void(0)" class="uppercase">View All Products</a>
-    </div>
-    <!-- /.box-footer -->
 </div>
+
+<script>
+
+    var tableP = $("#table-procesos").DataTable({
+        ajax: "proceso/listarProcesos",
+        columns: [
+            {data: "_id_caso", visible: false},
+            {data: "_dni"},
+            {data: "_radicado"},
+            {data: "_descripcion"},
+            {data: "_fecha_fin"}
+        ]
+    });
+
+
+</script>
