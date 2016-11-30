06d083ce74e7a5861dbee0725203324807d1049f
marko0925 <marlonyessid0925@gmail.com>
2016-11-29 19:53:19 -0500

    listar proceso js

--- public/dist/js/proceso/listarProcesos.js
+++ public/dist/js/proceso/listarProcesos.js
@@ -2,8 +2,8 @@
  * Created by McBro on 22/11/2016.
  */
 
-$("listarProcesos").on("click",function () {
+$("#listarProcesos").on("click",function () {
     $(".content-header").html("<h1>Procesos Existentes</h1>");
-    abrirVista("proceso/listadoProcesos");
+    abrirVista("proceso/formularioListarProcesos");
 });
 
