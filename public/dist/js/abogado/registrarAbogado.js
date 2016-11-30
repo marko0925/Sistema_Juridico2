b017054e190c5363b1046144f7b1f82a473821ae
marko0925 <marlonyessid0925@gmail.com>
2016-11-29 19:51:03 -0500

    registrar abogado js

--- public/dist/js/abogado/registrarAbogado.js
+++ public/dist/js/abogado/registrarAbogado.js
@@ -21,21 +21,22 @@ $("#ctrl-tabs").on("click", function () {
  * Se encarga de añadir especializacion a la tabla
  */
 function anadirEspec() {
-    var nombre = $("input[name=txtEspecialidadAbogado]").val();
+    var nombre = $("#listEsp").val();
+    var descripcion = $("#descripciontxt").val();
     var fecha = $("input[name=txtFechaActaAbogado]").val();
     var universidad = $("input[name= txtUniversidadActaAbogado]").val();
-    var acta = $("input[name=acta]").val();
     console.log(nombre);
     var t = $('#tabla-especialidades').DataTable();
     t.row.add([
         nombre,
         fecha,
         universidad,
-        acta
+        descripcion
     ]).draw();
 
 
 }
+
 function tomarInfo() {
     var dni = $("input[name=txtDniAbogado]").val();
     var nombre = $("input[name=txtNombreAbogado]").val();
@@ -60,28 +61,32 @@ function tomarInfo() {
     };
 }
 function actualizarA() {
+    if (info.dni == "" || info.nombre == "" || info.apellido == "" || info.correo == "" || info.clave == "" || info.fechaNac == "" || info.telefono == "" ||
+        info.almamater == "" || info.especialidades == undefined) {
 
-    var info = tomarInfo();
-    $.ajax({
-        type: "POST",
-        url: "abogado/actualizar",
-        data: info,
-        success:  function(){
-            $("#msj").html('<div class="alert alert-success alert-dismissible">' +
-                '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>' +
-                '<h4><i class="icon fa fa-check"></i>Actualizado correctamente</h4>' +
-                'Se ha actualizado con exito el abogado '+info.nombre+"." +
-                '</div>');
-            limpiarTextAbogado();
-        },
-        error:function(err){
-            $("#msj").html('<div class="alert alert-danger alert-dismissible">' +
-                '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
-                '<h4><i class="icon fa fa-ban"></i> ¡Ups!</h4>' +
-                'Algo ha ido mal, comprueba tu conexion a internet.' +
-                '</div>');
-        }
-    });
+    } else {
+        var info = tomarInfo();
+        $.ajax({
+            type: "POST",
+            url: "abogado/actualizar",
+            data: info,
+            success: function () {
+                $("#msj").html('<div class="alert alert-success alert-dismissible">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>' +
+                    '<h4><i class="icon fa fa-check"></i>Actualizado correctamente</h4>' +
+                    'Se ha actualizado con exito el abogado ' + info.nombre + "." +
+                    '</div>');
+                limpiarTextAbogado();
+            },
+            error: function (err) {
+                $("#msj").html('<div class="alert alert-danger alert-dismissible">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
+                    '<h4><i class="icon fa fa-ban"></i> ¡Ups!</h4>' +
+                    'Algo ha ido mal, comprueba tu conexion a internet.' +
+                    '</div>');
+            }
+        });
+    }
 }
 function RAbogado() {
 
@@ -89,39 +94,41 @@ function RAbogado() {
     if (info.dni == "" || info.nombre == "" || info.apellido == "" || info.correo == "" || info.clave == "" || info.fechaNac == "" || info.telefono == "" ||
         info.almamater == "" || info.especialidades == undefined) {
 
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "abogado/registrar",
+            data: info,
+            success: function (data) {
+                $("#msj").html('<div class="alert alert-success alert-dismissible">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>' +
+                    '<h4><i class="icon fa fa-check"></i>Registro correctamente</h4>' +
+                    'Se ha registrado con exito el abogado ' + info.nombre + "." +
+                    '</div>');
+                limpiarTextAbogado();
+            },
+            error: function (err) {
+                $("#msj").html('<div class="alert alert-danger alert-dismissible">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
+                    '<h4><i class="icon fa fa-ban"></i> ¡Ups!</h4>' +
+                    'Algo ha ido mal, comprueba tu conexion a internet.' +
+                    '</div>');
+
+
+            }
+        });
     }
-    $.ajax({
-        type: "POST",
-        url: "abogado/registrar",
-        data: info,
-        success: function (data) {
-            $("#msj").html('<div class="alert alert-success alert-dismissible">' +
-                '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>' +
-                '<h4><i class="icon fa fa-check"></i>Registro correctamente</h4>' +
-                'Se ha registrado con exito el abogado '+info.nombre+"." +
-                '</div>');
-            limpiarTextAbogado();
-        },
-        error: function (err) {
-            $("#msj").html('<div class="alert alert-danger alert-dismissible">' +
-                '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' +
-                '<h4><i class="icon fa fa-ban"></i> ¡Ups!</h4>' +
-                'Algo ha ido mal, comprueba tu conexion a internet.' +
-                '</div>');
 
-
-        }
-    });
 }
 
 function limpiarTextAbogado() {
     $("input[name=txtDniAbogado]").val("");
     $("input[name=txtNombreAbogado]").val("");
-     $("input[name=txtApellidoAbogado]").val("");
-     $("input[name=txtCorreoAbogado]").val("");
-     $("input[name=txtPassAbogado]").val("");
-     $("input[name=txtFechaNacimientoAbogado]").val("");
-     $("input[name=txtTelefonoAbogado]").val("");
+    $("input[name=txtApellidoAbogado]").val("");
+    $("input[name=txtCorreoAbogado]").val("");
+    $("input[name=txtPassAbogado]").val("");
+    $("input[name=txtFechaNacimientoAbogado]").val("");
+    $("input[name=txtTelefonoAbogado]").val("");
     $("input[name=txtAlmamaterAbogado]").val("");
     $("#tabla-especialidades").dataTable().clear().draw();
 }
