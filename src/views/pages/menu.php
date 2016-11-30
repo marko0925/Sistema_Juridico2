94343d51b50249d36eac910c038a3eb1dedef728
marko0925 <marlonyessid0925@gmail.com>
2016-11-29 19:35:15 -0500

    menu actualizado

--- src/views/pages/menu.php
+++ src/views/pages/menu.php
@@ -12,28 +12,20 @@
             </div>
         </div>
         <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
+
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
-            <li class="header">MAIN NAVIGATION</li>
+            <li class="header">Menu</li>
             <li class="treeview">
                 <a href="#">
-                    <i class="fa fa-dashboard"></i> <span>Procesos</span>
+                    <i class="fa fa-gavel"></i> <span>Procesos</span>
                     <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li class="active"><a href="#" id="registrarProceso"><i class="fa fa-circle-o"></i> Registrar</a>
+                    <li class="active"><a href="#" id="registrarProceso"><i class="fa fa-circle-o"></i>Registrar</a>
                     </li>
                     <li><a href="#" id="listarProcesos"><i class="fa fa-circle-o"></i> Listar</a></li>
                     <li><a href="#"><i class="fa fa-circle-o"></i> Observaciones</a></li>
@@ -42,10 +34,10 @@
             </li>
             <li class="treeview">
                 <a href="#">
-                    <i class="fa fa-files-o"></i>
+                    <i class="fa  fa-graduation-cap"></i>
                     <span>Abogados</span>
                     <span class="pull-right-container">
-              <span class="label label-primary pull-right">4</span>
+              <i class="fa fa-angle-left pull-right"></i>
             </span>
                 </a>
                 <ul class="treeview-menu">
@@ -57,7 +49,7 @@
 
             <li class="treeview">
                 <a href="#">
-                    <i class="fa fa-pie-chart"></i>
+                    <i class="fa fa-user"></i>
                     <span>Clientes</span>
                     <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
